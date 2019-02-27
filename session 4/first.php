afddc6b16cefba8bcc014c3cee80833799d4760e
mahdi <asadi.mahdi@gmail.com>
2019-02-27 11:42:54 +0330

    php introduction

---
+++ session 4/first.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "hello world";
+?>
+<br>
+<p>    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum atque minima similique praesentium vitae recusandae quos, doloribus saepe deserunt nam impedit ut nobis cum animi in ratione illo placeat dignissimos.</p>
+<?php
+$x =4;
+if($x ===4)
+{
+    echo "ایول";
+}
+else
+{
+    echo " ای بابا";
+}
+?> 
+</body>
+</html>
+
+
